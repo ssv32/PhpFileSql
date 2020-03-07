d14d0d6aaf5afe06d1d6499f66aa36c455363c45
ssv32 <ssv_32@mail.ru>
2020-03-07 21:48:19 +0600

    add text in index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,19 +1,28 @@
 <? 
 /**
  * PhpFileSql 
- * - мини база данных на файлах, ниже примеры использования
+ * - мини система управления базами данных (БД), 
+ *  БД представляются в виде текстовых, зашифрованных файлов.
+ * Всё написано чисто на php не требует зависимостей, для работы нужен один класс.
+ * 
+ * Ниже примеры использования
+ * 
+ * сокращения везде ниже:
+ *  БД - база данных
 */
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// класс для рабты с БД на файлах 
+// класс для работы с БД на файлах 
 include_once('./classes/PhpFileSql.php');
+
 
 // путь до папки в которой должны быть файлы БД 
 //  (! желательно что бы он указывал за веб пространство проекта)
-$urlFileDb = '/var/www/dbPhpFileSql/';
+$urlFileDb = 'C:/OSPanel/domains/php-file-sql.lc/dbPhpFileSql/';
+//$urlFileDb = '/var/www/dbPhpFileSql/';
 
 $phpFileSql = new PhpFileSql($urlFileDb);
 
@@ -23,27 +32,34 @@ $nameDataBase = 'gy';
 
 $phpFileSql->connect($login, $pass, $nameDataBase);
 
+// закрыть текущее подключение к БД
 //$phpFileSql->close();
 
+// сохранить текущую состояние БД в файл 
+//  (сохранение всегда происходит когда объект класса PhpFileSql уничтожается)
 //$phpFileSql->saveThisDbInFile();
 
-//$phpFileSql->showErrors();
-
-//$phpFileSql->close();
+// создать БД (файл БД) и подключиться к ней, методом connect
 //$phpFileSql->createDataBase('asd', 'asdasd', 'zxc22' );
 
+// удалить БД
 //$phpFileSql->deleteDataBase('asd', 'asdasd', 'zxc22');
 
-
+// получить массив со всеми имеющимися БД
 //$res = $phpFileSql->getDataBases();
 
+// создать таблицу <имя таблицы>, <массив с названиями столбцов> 
+//  (пока все столбцы типа строка)
 //$phpFileSql->createTable('asd2', array('login', 'pass', 'flag'));
 
-//$phpFileSql->dropTable('asd2');
+// удалить таблицу
 //$phpFileSql->dropTable('asd2');
 
+// переименовать таблицу
 //$phpFileSql->renameTable('asd', 'asd3');
 
+// вставить значение (строку в таблицу), 
+//  не указанные столбцы будут заполнены пустыми значениями
 //$phpFileSql->insertInto(
 //    'asd3', 
 //    array(
@@ -52,6 +68,7 @@ $phpFileSql->connect($login, $pass, $nameDataBase);
 //    )
 //);
 
+// обновить запись/строку в таблицы (используется 1 вариант условия выборки)
 //$phpFileSql->update(
 //    'asd3', 
 //    array(
@@ -66,6 +83,7 @@ $phpFileSql->connect($login, $pass, $nameDataBase);
 //    ) 
 //);
 
+// обновить запись/строку в таблицы (используется 2 вариант условия выборки)
 //$phpFileSql->update(
 //    'asd3', 
 //    array(
@@ -86,6 +104,7 @@ $phpFileSql->connect($login, $pass, $nameDataBase);
 //    ) 
 //);
 
+// удалить запись в таблицы
 //$phpFileSql->delete(
 //    'asd3',
 //    array(
@@ -96,21 +115,22 @@ $phpFileSql->connect($login, $pass, $nameDataBase);
 //    )   
 //);
 
+// вернёт массив со всеми имеющимися в БД таблицами 
 $res = $phpFileSql->getAllTables();
 
 echo 'res <pre>';
 print_r($res);
 echo '</pre>';
 
-//echo 'datasDataBase <pre>';
-//print_r($phpFileSql->datasDataBase);
-//echo '</pre>';
-
-
+// выведет текущую ошибку
 $phpFileSql->showErrors();
 
+// добавить новый столбец в таблицу
 //$phpFileSql->alterTable('testTable', array( 'asd' , 'asd2'));
 
+// выбрать записи из таблицы, используется 1 вариант условия выборки
+//  (всего доступны два варианта условия)
+//  метод вернёт массив со всеми значениями
 //$res = $phpFileSql->select(
 //    'testTable', 
 //    '*', 
@@ -122,6 +142,9 @@ $phpFileSql->showErrors();
 //    ) 
 //);
 
+// выбрать записи из таблицы, используется 2 вариант условия выборки 
+//  (всего доступны два варианта условия)
+//   метод вернёт массив со всеми значениями
 //$res = $phpFileSql->select(
 //    'testTable', 
 //    '*', 
@@ -139,10 +162,6 @@ $phpFileSql->showErrors();
 //            
 //    ) 
 //);
-
-//echo 'select $res<pre>';
-//print_r($res);
-//echo '</pre>';
 
 
 
